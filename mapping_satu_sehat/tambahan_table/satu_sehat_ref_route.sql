95b01373fdc21003c5ab1f80dbb44b667b7a40c4
ichsanleonhart <39687405+ichsanleonhart@users.noreply.github.com>
2026-04-06 06:53:08 +0700

    Tambahan data di table referensi Mapping Satu Sehat

--- mapping_satu_sehat/tambahan_table/satu_sehat_ref_route.sql
+++ mapping_satu_sehat/tambahan_table/satu_sehat_ref_route.sql
@@ -14,12 +14,36 @@ CREATE TABLE `satu_sehat_ref_route`  (
 -- ----------------------------
 -- Records of satu_sehat_ref_route
 -- ----------------------------
-INSERT INTO `satu_sehat_ref_route` VALUES ('Inhal', 'Inhalasi (Uap)');
-INSERT INTO `satu_sehat_ref_route` VALUES ('N', 'Nasal (Hidung)');
-INSERT INTO `satu_sehat_ref_route` VALUES ('O', 'Oral (Minum)');
-INSERT INTO `satu_sehat_ref_route` VALUES ('P', 'Parenteral (Suntik/Infus)');
-INSERT INTO `satu_sehat_ref_route` VALUES ('R', 'Rectal (Anus)');
+INSERT INTO `satu_sehat_ref_route` VALUES ('Chewing Gum', 'Chewing Gum');
+INSERT INTO `satu_sehat_ref_route` VALUES ('cutaneous', 'Cutaneous');
+INSERT INTO `satu_sehat_ref_route` VALUES ('implant', 'Implant');
+INSERT INTO `satu_sehat_ref_route` VALUES ('Inhal', 'Inhalation');
+INSERT INTO `satu_sehat_ref_route` VALUES ('Inhal.aerosol', 'Inhalation Aerosol');
+INSERT INTO `satu_sehat_ref_route` VALUES ('Inhal.powder', 'Inhalation Powder');
+INSERT INTO `satu_sehat_ref_route` VALUES ('Inhal.solution', 'Inhalation Solution');
+INSERT INTO `satu_sehat_ref_route` VALUES ('inj.intramuscular', 'Injection Intramuscular');
+INSERT INTO `satu_sehat_ref_route` VALUES ('inj.intrathecal', 'Injection Intrathecal');
+INSERT INTO `satu_sehat_ref_route` VALUES ('inj.intravenous', 'Injection Intravenous');
+INSERT INTO `satu_sehat_ref_route` VALUES ('inj.subcutaneous', 'Injection Subcutaneous');
+INSERT INTO `satu_sehat_ref_route` VALUES ('Instill', 'Instillation');
+INSERT INTO `satu_sehat_ref_route` VALUES ('Instill.solution', 'Instillation Solution');
+INSERT INTO `satu_sehat_ref_route` VALUES ('intravesical', 'Intravesical');
+INSERT INTO `satu_sehat_ref_route` VALUES ('lamella', 'Lamella');
+INSERT INTO `satu_sehat_ref_route` VALUES ('N', 'Nasal');
+INSERT INTO `satu_sehat_ref_route` VALUES ('O', 'Oral');
+INSERT INTO `satu_sehat_ref_route` VALUES ('ocular', 'Ocular');
+INSERT INTO `satu_sehat_ref_route` VALUES ('ointment', 'Ointment');
+INSERT INTO `satu_sehat_ref_route` VALUES ('oral aerosol', 'Oral Aerosol');
+INSERT INTO `satu_sehat_ref_route` VALUES ('otic', 'Otic');
+INSERT INTO `satu_sehat_ref_route` VALUES ('P', 'Parenteral');
+INSERT INTO `satu_sehat_ref_route` VALUES ('R', 'Rectal');
+INSERT INTO `satu_sehat_ref_route` VALUES ('s.c. implant', 'S.C. Implant');
+INSERT INTO `satu_sehat_ref_route` VALUES ('SL', 'Sublingual/Buccal/Oro mucosal');
+INSERT INTO `satu_sehat_ref_route` VALUES ('stomatologic', 'stomatologic');
+INSERT INTO `satu_sehat_ref_route` VALUES ('TD', 'Transdermal');
+INSERT INTO `satu_sehat_ref_route` VALUES ('TD patch', 'Transdermal Patch');
 INSERT INTO `satu_sehat_ref_route` VALUES ('Topical', 'Topikal (Oles/Kulit/Mata/Telinga)');
+INSERT INTO `satu_sehat_ref_route` VALUES ('urethral', 'Urethral');
 INSERT INTO `satu_sehat_ref_route` VALUES ('V', 'Vaginal');
 
 SET FOREIGN_KEY_CHECKS = 1;
